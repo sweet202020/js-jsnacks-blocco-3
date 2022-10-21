2b974554d8ee5c5d3512d84c91c3b53d20aaba0d
sweet202020 <dolcealessio1@gmail.com>
2022-10-21 11:37:34 +0200

    array vuoto

---
+++ js-snack-1/script.js
@@ -0,0 +1,78 @@
+/* Crea un array composto da 10 automobili.
+Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+Infine stampa separatamente i 3 array. */
+
+const auto = [
+    {
+        marca: "fiat",
+        modello: "punto",
+        alimentazione: "benzina",
+
+    },
+    {
+        marca: "ford",
+        modello: "focus",
+        alimentazione: "gpl",
+    },
+    {
+        marca: "toyota",
+        modello: "rav4",
+        alimentazione: "diesel",
+    },
+    {
+        marca: "audi",
+        modello: "r8",
+        alimentazione: "diesel",
+    },
+    {
+        marca: "bmw",
+        modello: "x5",
+        alimentazione: "gpl",
+    },
+    {
+        marca: "mercedes",
+        modello: "benz classe a",
+        alimentazione: "elettrico",
+    },
+    {
+        marca: "jaguar",
+        modello: "pace",
+        alimentazione: "diesel",
+    },
+    {
+        marca: "ferrari",
+        modello: "roma",
+        alimentazione: "gpl",
+    },
+    {
+        marca: "alfa romeo",
+        modello: "giulietta",
+        alimentazione: "metano",
+    },
+    {
+        marca: "tesla",
+        modello: "model y",
+        alimentazione: "elettrico",
+    },
+   
+]
+
+/* const benzinaArray= auto.filter(autoaBenzina)
+
+function autoaBenzina(){
+    if (auto.alimentazione==="diesel"){
+        console.log(auto.marca)
+       return auto.marca
+        
+    }
+} 
+console.log(benzinaArray) */
+
+const benzinaArray = auto.filter(autoBenzina=>{
+    if (auto.alimentazione==="benzina"){
+        return autoBenzina.marca
+        }
+})
+
+console.log(benzinaArray)
